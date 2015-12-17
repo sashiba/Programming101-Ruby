7bb4a54c0da57a631e86ccf4c161035179547c19
sashiba <sashopirnarev@gmail.com>
2015-12-17 13:30:41 +0200

    game of life

--- week04/1-game-of-life/solution.rb
+++ week04/1-game-of-life/solution.rb
@@ -1,57 +1,60 @@
-class Board
-  include Enumerable
+module GameOfLife
+  class Board
+    include Enumerable
+      
+    def initialize(*cells)
+      @cells = cells.uniq
+    end
+
+    def each(&block)
+      @cells.each(&block)
+    end
+
+    def neighbours(member)
+      x, y = member
+      neighbours = [ [x - 1, y + 1], [x, y + 1], [x + 1, y + 1],
+                     [x - 1, y],                 [x + 1, y],
+                     [x - 1, y - 1], [x, y - 1], [x + 1, y - 1]
+      ]
+    end
     
-  def initialize(*cells)
-    @cells = cells    
-  end
+    def [](x, y)
+      include? [x, y]
+    end
 
-  def each(&block)
-    @cells.each(&block)
-  end
-
-  def neighbours(member)
-    x, y = member
-    neighbours = [
-      [x - 1, y],
-      [x + 1, y],
-      [x, y - 1],
-      [x, y + 1],
-      [x - 1, y + 1],
-      [x + 1, y + 1],
-      [x - 1, y - 1],
-      [x + 1, y - 1]
-    ]
-  end
-
-  def [](x, y)
-    include? [x, y]
-  end
-
-  def revive(member)
-    count_live_neighbours(member) == 3
-  end
-
-  def count_live_neighbours(cell)
-    neighbours(cell).select { |neighbour| include?(neighbour) }.size
-  end
-
-  def live_cells
-    select { |cell| (2..3).include? count_live_neighbours(cell) }
-  end
-
-  def <<(other)
-    @cells << other unless @cells.include? other
-  end
-
-  def count
-    @cells.size
-  end
-
-  def next_generation
-    Board.new.tap do |next_gen|
-      @cells.each do |cell|
+    def revive_cell(cell_neighbours)
+      [].tap do |revived_cells|
+        cell_neighbours.each do |neighbour|
+          revived_cells << neighbour if (count_alive_neighbours(neighbour) == 3) and !include?(neighbour)
+        end
       end
     end
+
+    def count_alive_neighbours(cell)
+      neighbours(cell).select { |neighbour| include?(neighbour) }.size
+    end
+
+    def alive_cells
+      select { |cell| (2..3).include? count_alive_neighbours(cell) }
+    end
+
+    def <<(other)
+      @cells << other unless @cells.include? other
+    end
+
+    def count
+      @cells.size
+    end
+
+    def next_generation
+      Board.new.tap do |next_gen|
+        @cells.each do |cell|
+          revive_cell(neighbours(cell)).each { |res| next_gen << res }
+        end
+        next_gen << alive_cells.flatten(1)
+      end
+    end
+    
   end
 
 end
--- week04/1-game-of-life/solution_test.rb
+++ week04/1-game-of-life/solution_test.rb
@@ -6,4 +6,43 @@ class SolutionTest < Minitest::Test
   def test_the_truth
     assert true
   end
+
+  def test_indexing_true
+    board = GameOfLife::Board.new [1, 2], [1, 3], [5, 6]
+
+    assert_equal true, board[1, 2]
+  end
+
+  def test_indexing_false
+    board = GameOfLife::Board.new [1, 2], [1, 3], [5, 6]
+
+    assert_equal false, board[0, 2]
+  end
+
+  def test_count
+    board = GameOfLife::Board.new [1, 2], [1, 3], [5, 6]
+
+    assert_equal 3, board.count
+  end
+
+  def test_next_generation_with_revived_cell
+    board = GameOfLife::Board.new [1, 2], [1, 3], [1, 4]
+    next_gen = board.next_generation
+
+    assert_equal true, next_gen[0, 3]
+  end
+
+  def test_next_generation_with_dead_cell
+    board = GameOfLife::Board.new [1, 2], [1, 3], [1, 4]
+    next_gen = board.next_generation
+
+    assert_equal false, next_gen[1, 2]
+  end
+
+  def test_next_generation_count
+    board = GameOfLife::Board.new [1, 2], [1, 3], [1, 4]
+    next_gen = board.next_generation
+
+    assert_equal 3, next_gen.count
+  end
 end
