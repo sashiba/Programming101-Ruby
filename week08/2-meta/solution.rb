7d64d9f5e4ac0f00aca6f9c4207abc222b77d04a
Alexander Pirnarev <sashopirnarev@gmail.com>
2016-01-21 19:30:09 +0200

    2-meta/solution

--- week08/2-meta/solution.rb
+++ week08/2-meta/solution.rb
@@ -15,9 +15,33 @@ class Object
     end
   end
 
-  def define_singleton_method_v1(symbol, method, &block)
-    class << self
-      define_method(symbol.to_sym, instance_method(:method))
+  def define_singleton_method_v1(symbol, method = nil, &block)
+    # self.instance_eval
+    singleton_class.class_eval { define_method(symbol, method) } if method
+    singleton_class.class_eval { define_method(symbol, &block) } if block_given?
+  end
+end
+
+class String
+  def decompose
+    methods = []
+    chars = self.chars
+    while chars.include? '.'
+      methods << chars.take_while { |c| c != '.' }.join
+      chars = chars.drop_while { |c| c != '.' }.drop(1)
     end
+    methods << chars.join if chars
+
+    methods
+  end
+
+  def to_proc
+    methods = self.decompose
+    p methods
+    methods.each do |method|
+      lambda { |x, *args| x.public_send method.to_sym, *args }
+    end
+
+    #lambda { |x, *args| x.public_send self, *args }
   end
 end
